d443776248d3bb461141035e898252bc109bb17e
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-09 13:34:55 -0400

    Update RemoteOffsetDumper.lua

--- utility/RemoteOffsetDumper.lua
+++ utility/RemoteOffsetDumper.lua
@@ -1,10 +1,7 @@
 local console = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Console/main/src/main.lua"))():Init(true, "Remote-Offset Dumper");
-local blacklist = ("OnNewMessage" or "OnMessageDoneFiltering" or "OnNewSystemMessage" or "OnChannelJoined" or "OnChannelLeft" or "OnMuted" or "OnUnmuted" or "OnMainChannelSet" or "ChannelNameColorUpdated" or "SayMessageRequest" or "GetInitDataRequest" or "MutePlayerRequest" or "UnMutePlayerRequest")
-
---> Blacklist Soon
 
 if rconsoleprint then
-    console.log("local remote_offsets = {", "blue");
+    console.log("local offsets = {", "blue");
 
     for _i, offset in pairs(game.ReplicatedStorage:GetDescendants()) do
         local OffsetName = string.lower(offset.Name);
@@ -12,11 +9,17 @@ if rconsoleprint then
         local OffsetPath = offset:GetFullName();
 
         if offset:IsA("BindableEvent") then
-            console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> BindableEvent", "light_green");
+            if offset.Parent ~= game.ReplicatedStorage.DefaultChatSystemChatEvents then
+                console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> BindableEvent", "light_green");
+            end;
         elseif offset:IsA("RemoteEvent") then
-            console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> RemoteEvent", "yellow");
+            if offset.Parent ~= game.ReplicatedStorage.DefaultChatSystemChatEvents then
+                console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> RemoteEvent", "yellow");
+            end;
         elseif offset:IsA("RemoteFunction") then
-            console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> RemoteFunction", "light_magenta");
+            if offset.Parent ~= game.ReplicatedStorage.DefaultChatSystemChatEvents then
+                console.log("    " .. OffsetName .. " = game." .. OffsetPath .. "[\"" .. FullOffsetName .. "\"]; --> RemoteFunction", "light_magenta");
+            end;
         end;
     end;
 
