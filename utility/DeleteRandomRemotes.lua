8deb4a49767beefd5de7ed52bdedf2a5518e23ab
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-12-21 09:23:09 -0500

    Update with blacklist

--- utility/DeleteRandomRemotes.lua
+++ utility/DeleteRandomRemotes.lua
@@ -1,5 +1,9 @@
+--> Made for specific game, edit is now universal.
+
+local Blacklist = { "TestRemote" }
+
 for i,v in pairs(game:GetService("ReplicatedStorage"):GetChildren()) do
-	if v:IsA("RemoteEvent") then
-		v:Destroy();
-	end;
-end;
+    if (v:IsA("RemoteEvent") or v:IsA("RemoteFunction")) and table.find(Blacklist, v.Name) then
+        v:Destroy()
+    end
+end
