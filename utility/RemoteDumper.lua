408ee3d62acad969c34e7fd8f77549d373396532
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-01-11 07:47:19 -0500

    Rename and Update
    Shorter + Better

---
+++ utility/RemoteDumper.lua
@@ -0,0 +1,17 @@
+assert(rconsoleprint, "'rconsoleprint' is not supported by your exploit")
+
+rconsoleprint("local remotes = {")
+
+for _, remote in pairs(game:GetService("ReplicatedStorage"):GetDescendants()) do
+    local RemoteName = string.lower(remote.Name)
+    local FullRemoteName = remote.Name
+    local RemotePath = remote:GetFullName()
+
+    if remote.Parent ~= game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents then
+        if remote:IsA("RemoteEvent") or remote:IsA("RemoteFunction") then
+            rconsoleprint("    [\"" .. RemoteName .. "\"] = game." .. RemotePath .. "[\"" .. FullRemoteName .. "\"]; \n")
+        end
+    end
+end
+
+rconsoleprint("}\n")
