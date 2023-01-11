e3e9a66679110baf38468929c940dad5cd76cc79
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-01-11 09:43:32 -0500

    Added `Comment(text)`

--- utility/Markdown.lua
+++ utility/Markdown.lua
@@ -1,21 +1,27 @@
 --> Used to create and edit MD files, placeholder and super primitive!
+--> Written for luau (base)
+--> NOT WITH EXPLOITS IN MIND
 
 local function format(...)
     return tostring(...) .. "\n"
 end
 
---> The idea is super super simple, just a bunch of functions to make writing MD files in lua cooler!!
---> Written for Exploits (Syn, SW, etc) will make one for base luau (latest ver)
-function Header(text, level)
+function Comment(text: string)
+	local text = format(text)
+
+	print("<!-- " .. text .. " -->")
+end
+
+function Header(text: string, level: number)
     local level = level or 1
     local text = format(text)
     
     if level == 1 then
-        return "# " .. text
+        print("# " .. text)
     elseif level == 2 then
-        return "## " .. text
+        print("## " .. text)
     elseif level == 3 then
-        return "### " .. text
+        print("### " .. text)
     else
         error("'level' is an invalid number")
         return
