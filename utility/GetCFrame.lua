03673fa2aad06dfcf2a899ba7c621e76c0f7e2b7
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-01-12 14:07:48 -0500

    Update GetCFrame.lua

--- utility/GetCFrame.lua
+++ utility/GetCFrame.lua
@@ -1,4 +1,4 @@
 local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
 
-print(HumanoidRootPart.CFrame.X .. ", " .. HumanoidRootPart.CFrame.Y .. ", " .. HumanoidRootPart.CFrame.Z)
-setclipboard("CFrame.new(" .. tostring(math.floor(HumanoidRootPart.CFrame.X) .. ", " .. math.floor(HumanoidRootPart.CFrame.Y) .. ", " .. math.floor(HumanoidRootPart.CFrame.Z)) .. ")")
+print("Copied: " .. HumanoidRootPart.CFrame.X .. ", " .. HumanoidRootPart.CFrame.Y .. ", " .. HumanoidRootPart.CFrame.Z)
+setclipboard("game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.new(" .. tostring(math.floor(HumanoidRootPart.CFrame.X) .. ", " .. math.floor(HumanoidRootPart.CFrame.Y) .. ", " .. math.floor(HumanoidRootPart.CFrame.Z)) .. ")")
